5632d8c965acce917d4e5d5dfe63f40820873968
Jay Little <jaylittle@jaylittle.com>
2020-02-07 16:56:56 -0500

    Tweaked stream script settings a bit.

--- ffmpeg/ffmpeg-config-sample.sh
+++ ffmpeg/ffmpeg-config-sample.sh
@@ -4,18 +4,23 @@
 
 INRES="1280x720" # input resolution
 OUTRES="1280x720" # output resolution
-FPS="30" # target FPS
+IN_FPS="30" # input FPS
+OUT_FPS="30" # target FPS
 GOP="60" # i-frame interval, should be double of FPS, 
 GOPMIN="30" # min i-frame interval, should be equal to fps,
-CBR="3000k" # constant bitrate (should be between 1000k - 3000k)
+MIN_BR="3000k" # minimum bitrate (should be between 1000k - 3000k)
+AVG_BR="3000k" # average bitrate (should be between 1000k - 3000k)
+MAX_BR="3000k" # maximum bitrate (should be between 1000k - 3000k)
 OUTAUDIO_FORMAT="aac"
 OUTAUDIO_RATE="44100"
 OUTAUDIO_CHANNELS="2"
+OUTAUDIO_RATE="128k"
 OUTVIDEO_CODEC="h264_omx"
-OUTVIDEO_QUALITY="ultrafast"
+OUTVIDEO_PRESET="ultrafast"
+OUTVIDEO_TUNE="zerolatency"
 OUTVIDEO_PIXFMT="yuv420p"
 OUTSTREAM_CONTAINER="flv"
-OUTSTREAM_THREADS="2"
+OUTSTREAM_THREADS="4"
 STREAM_KEY="1234567890"
 STREAM_SERVER="live-atl"
 STREAM_URL="rtmp://$STREAM_SERVER.twitch.tv/app/$STREAM_KEY"
--- ffmpeg/stream-video-input-audio-inputs.sh
+++ ffmpeg/stream-video-input-audio-inputs.sh
@@ -13,8 +13,9 @@ ffmpeg -v verbose \
 -itsoffset $INAUDIO1_DELAY -f $INAUDIO1_FORMAT -ac $INAUDIO1_CHANNELS -i $INAUDIO1_DEVICE \
 -itsoffset $INAUDIO2_DELAY -f $INAUDIO2_FORMAT -ac $INAUDIO2_CHANNELS -i $INAUDIO2_DEVICE \
 -filter_complex "[0:0]volume=$INAUDIO1_VOLUME[a];[1:0]volume=$INAUDIO2_VOLUME[b];[a][b]amix=inputs=2:duration=longest" \
--f $INVIDEO1_FORMAT -framerate $FPS -video_size $INRES -i $INVIDEO1_DEVICE \
--vcodec $OUTVIDEO_CODEC -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR \
--pix_fmt $OUTVIDEO_PIXFMT -s $OUTRES \
+-f $INVIDEO1_FORMAT -framerate $IN_FPS -video_size $INRES -i $INVIDEO1_DEVICE \
+-vcodec $OUTVIDEO_CODEC -g $GOP -keyint_min $GOPMIN -b:v $AVG_BR \
+-minrate:v $MIN_BR -maxrate:v $MAX_BR -pix_fmt $OUTVIDEO_PIXFMT -s $OUTRES \
 -c:a $OUTAUDIO_FORMAT -f $OUTSTREAM_CONTAINER -ac $OUTAUDIO_CHANNELS -ar $OUTAUDIO_RATE \
--threads $OUTSTREAM_THREADS -bufsize $CBR -r $FPS -vsync 1 -async 1 "$STREAM_URL"
+-b:a $OUTAUDIO_RATE -threads $OUTSTREAM_THREADS -bufsize $MAX_BR -r $OUT_FPS -vsync 1 -async 1 \
+-preset $OUTVIDEO_PRESET -tune $OUTVIDEO_TUNE "$STREAM_URL"
