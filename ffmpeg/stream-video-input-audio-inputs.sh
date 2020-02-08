134f2973f0c5afddc908815c9668f775771c2a24
Jay Little <jaylittle@jaylittle.com>
2020-02-08 09:12:15 -0500

    Removed ar flag from ffmpeg command

--- ffmpeg/stream-video-input-audio-inputs.sh
+++ ffmpeg/stream-video-input-audio-inputs.sh
@@ -16,6 +16,6 @@ ffmpeg -v verbose \
 -f $INVIDEO1_FORMAT -framerate $IN_FPS -video_size $INRES -i $INVIDEO1_DEVICE \
 -vcodec $OUTVIDEO_CODEC -g $GOP -keyint_min $GOPMIN -b:v $AVG_BR \
 -minrate:v $MIN_BR -maxrate:v $MAX_BR -pix_fmt $OUTVIDEO_PIXFMT -s $OUTRES \
--c:a $OUTAUDIO_FORMAT -f $OUTSTREAM_CONTAINER -ac $OUTAUDIO_CHANNELS -ar $OUTAUDIO_RATE \
+-c:a $OUTAUDIO_FORMAT -f $OUTSTREAM_CONTAINER -ac $OUTAUDIO_CHANNELS \
 -b:a $OUTAUDIO_RATE -threads $OUTSTREAM_THREADS -bufsize $MAX_BR -r $OUT_FPS -vsync 1 -async 1 \
 -preset $OUTVIDEO_PRESET -tune $OUTVIDEO_TUNE "$STREAM_URL"
