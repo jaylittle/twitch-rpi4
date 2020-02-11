7559d009476738a4ae345a0dc599780213144c48
Jay Little <jaylittle@jaylittle.com>
2020-02-11 17:04:19 -0500

    Replaced ALSA Hardware IDs which non-fungible Device Names.  Updated Mixer implementation so that it is easier to customize without changing the script.

--- ffmpeg/stream-video-input-audio-inputs-config-sample.sh
+++ ffmpeg/stream-video-input-audio-inputs-config-sample.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #NOTE: Copy this to stream-video-input-audio-inputs-config.sh and configure appropriately
 #NOTE: The Following Settings are Specific to this Script
+#WARNING: Consider using ALSA device names gathered from arecord -L so that settings will survive a reboot as device numbers like hw:2,0 can and will change
 
 INVIDEO1_FORMAT="video4linux2"
 INVIDEO1_DEVICE="/dev/video0"
-INAUDIO1_FORMAT="alsa"
-INAUDIO1_DEVICE="hw:2,0"
-INAUDIO1_VOLUME="0.04"
+INAUDIO1_FORMAT="alsa" #Tempting though it may be, avoid pulse like the plague on an RPI
+INAUDIO1_DEVICE="hw:CARD=Capture,DEV=0" #hw:2,0 = Capture Card Input
+INAUDIO1_VOLUME="0.15"
 INAUDIO1_DELAY="-00:00:00.250"
 INAUDIO1_CHANNELS="2"
 INAUDIO2_FORMAT="alsa"
-INAUDIO2_DEVICE="hw:1,0"
+INAUDIO2_DEVICE="hw:CARD=H,DEV=0" #hw:1,0 = Microphone Input
 INAUDIO2_VOLUME="1.00"
 INAUDIO2_DELAY="-00:00:00.250"
 INAUDIO2_CHANNELS="1"
+INAUDIO_FILTER="amix=inputs=2:duration=longest" #Feel free tweak this setting to whatever works for you using docs @ https://ffmpeg.org/ffmpeg-filters.html#amix
--- ffmpeg/stream-video-input-audio-inputs.sh
+++ ffmpeg/stream-video-input-audio-inputs.sh
@@ -12,7 +12,7 @@ fi
 ffmpeg -v verbose \
 -itsoffset $INAUDIO1_DELAY -f $INAUDIO1_FORMAT -ac $INAUDIO1_CHANNELS -i $INAUDIO1_DEVICE \
 -itsoffset $INAUDIO2_DELAY -f $INAUDIO2_FORMAT -ac $INAUDIO2_CHANNELS -i $INAUDIO2_DEVICE \
--filter_complex "[0:0]volume=$INAUDIO1_VOLUME[a];[1:0]volume=$INAUDIO2_VOLUME[b];[a][b]amix=inputs=2:duration=longest" \
+-filter_complex "[0:0]volume=$INAUDIO1_VOLUME[a];[1:0]volume=$INAUDIO2_VOLUME[b];[a][b]$INAUDIO_FILTER" \
 -f $INVIDEO1_FORMAT -framerate $IN_FPS -video_size $INRES -i $INVIDEO1_DEVICE \
 -vcodec $OUTVIDEO_CODEC -g $GOP -keyint_min $GOPMIN -b:v $AVG_BR \
 -minrate:v $MIN_BR -maxrate:v $MAX_BR -pix_fmt $OUTVIDEO_PIXFMT -s $OUTRES \
